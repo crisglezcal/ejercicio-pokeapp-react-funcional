bc9b092f9d0c6b422a73513392fe47dcb393d1d2
crisglezcal <glezcalcris95@gmail.com>
2025-11-27 17:23:30 +0100

    fixed/pokemoncard

--- src/components/Main/SearchContainer/PokemonList/PokemonCard/PokemonCard.jsx
+++ src/components/Main/SearchContainer/PokemonList/PokemonCard/PokemonCard.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 const PokemonCard = ({pokemon}) => {
+  const id = pokemon.id;
+  const name = pokemon.name;
+  const image= pokemon.sprites.front_default;
+  const typeOne= pokemon.types[0].type.name;
+  const typeTwo= pokemon.types[1]?.type.name;
   return <article>
-      <h3>{pokemon.name}</h3>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-    </article>
+    <h1>{name}</h1>
+    <p>{id}</p>
+    <img src={image}></img>
+    <p>{typeOne}</p>
+    <p>{typeTwo}</p>
+  </article>;
 };
-
 export default PokemonCard;
